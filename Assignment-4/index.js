bf06e6c51bd833a915828d30454b1a8d285b6cd9
sumbul10 <sumbulfatlma756@gmail.com>
2026-07-18 16:33:39 +0500

    Create Assignment

--- Assignment-4/index.js
+++ Assignment-4/index.js
@@ -8,35 +8,41 @@ let image = "";
 
 if (percentage >= 0 && percentage <= 100) {
 
-    if (percentage >= 80) {
+    if (percentage >= 90) {
         grade = "A<sup>+</sup>";
         status = "PASS";
         color = "#28a745";
         image = "image/congratulations.png";
     }
-    else if (percentage >= 70) {
+   else if (percentage >= 80) {
         grade = "A";
         status = "PASS";
         color = "#28a745";
         image = "image/Keep-it-up!.png";
     }
-    else if (percentage >= 60) {
+    else if (percentage >= 70) {
         grade = "B";
         status = "PASS";
         color = "#28a745";
         image = "image/Excellent-work!.png";
     }
-    else if (percentage >= 50) {
+    else if (percentage >= 60) {
         grade = "C";
         status = "PASS";
         color = "#28a745";
         image = "image/good.png";
     }
+    else if (percentage >= 40) {
+        grade = "D";
+        status = "PASS";
+        color = "#28a745";
+        image = "image/improve.png";
+    }
     else {
         grade = "F";
         status = "FAIL";
         color = "#dc3545";
-        image = "image/improve.png";
+        image = "image/fail.png";
     }
 
 }
